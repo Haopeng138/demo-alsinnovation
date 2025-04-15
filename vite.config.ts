0c9ee4cc5682a03ab135c0dffc3c270ed988c703
Joel Guevara <jguevara@alstp.com>
2025-04-15 14:34:39 +0200

    add server changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       '@': '/src',
     },
+  },
+  server: {
+    host: 'www.alsinnovation.com',
+    port: 5173,
   }
 })
